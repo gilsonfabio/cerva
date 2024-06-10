a2ae8f522d27956234856d71dc0cf57e9aa20891
Gilson <gilsonfabio@gmail.com>
2024-06-09 22:03:43 -0300

    Atualizacoes imagens

--- mobile/src/server/api.ts
+++ mobile/src/server/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://10.111.135.208:3333"
+    //baseURL: "http://10.111.135.208:3333"
+    baseURL: "http://192.168.0.247:3333"
 })
